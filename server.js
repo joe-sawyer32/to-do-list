fa35cbbd4842ab0237899416c6fc40f9712734a0
Joe <joe-sawyer32@users.noreply.github.com>
2017-06-28 14:13:34 -0500

    Functionally complete, needs styling

--- server.js
+++ server.js
@@ -6,21 +6,38 @@ const path = require("path");
 const mustacheExpress = require("mustache-express");
 const bodyParser = require("body-parser");
 
+var toDos = [];
+var toDones = [];
+var itemCount = 0;
+
+// SET ENGINE
 app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
 app.set("views", path.join(__dirname, "/public"));
 
+// MIDDLEWARE
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/", express.static(path.join(__dirname, "/public")));
 
+// ROUTES
 app.get("/", (request, response) => {
-  response.render("index");
+  response.render("index", { todoList: toDos, todoneList: toDones });
 });
 
-app.post("/", (request, response) => {
-  let todoItem = request.body;
-  console.log(todoItem);
-  response.render("index", { todo: todoItem });
+app.post("/todolist", (request, response) => {
+  request.body.id = itemCount++;
+  toDos.push(request.body);
+  response.redirect("/");
+});
+
+app.post("/todonelist", (request, response) => {
+  toDos.forEach((item, index) => {
+    if (item.id == request.body.id) {
+      toDones.push({ toDoneItem: item.toDoItem });
+      toDos.splice(index, 1);
+    }
+  });
+  response.redirect("/");
 });
 
 app.listen(port, () => {
